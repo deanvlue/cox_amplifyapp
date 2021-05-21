07092d135d6d9570a27fc25d6d818ad3145ee301
CarlosMunoz <jose.munoz2@coxautoinc.com>
2021-05-21 13:36:30 -0500

    First commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo192.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Metrics App
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
