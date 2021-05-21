b071591de39b7b9ebc81250dbbd22853e15bcc0d
CarlosMunoz <jose.munoz2@coxautoinc.com>
2021-05-21 14:46:35 -0500

    Added header changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import logo from './logo192.png';
 import './App.css';
+import {Helmet} from 'react-helmet';
 
 function App() {
   return (
     <div className="App">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Cox Metics App</title>
+        <link rel="icon" type="image/icon type" href="favicon.ico"/>
+      </Helmet>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
